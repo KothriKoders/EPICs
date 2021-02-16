24db879f3c0a2393f8e4860fcb4c5e00f05c0a8a
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-02-16 14:33:16 +0530

    Update views.py

--- Project Mistletoe/Primis/accounts/views.py
+++ Project Mistletoe/Primis/accounts/views.py
@@ -19,19 +19,6 @@ def logout_view(request):
     return redirect('/')
     
     
-    
-    # for submission of the files ( to be checked)
-def index(request):
-      if request.method == 'POST':
-            student = StudentForm(request.POST, request.FILES)
-            student = StudentForm(request.POST, request.FILES)  
-       if student.is_valid():  
-            handle_uploaded_file(request.FILES['file'])  
-            model_instance = student.save(commit=False)
-            model_instance.save()
-            return HttpResponse("File uploaded successfuly")  
-       else:  
-          student = StudentForm()  
-           return render(request,"index.html",{'form':student})  
+
 
     
