e624ec4c691b4ed5e68c3c23d57173e0e23d840c
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-02-16 14:35:51 +0530

    Update function.py

--- Project Mistletoe/Primis/function.py
+++ Project Mistletoe/Primis/function.py
@@ -1,4 +1,4 @@
 def handle_upload_file(f):
-    with open('app/static/upload' +f.name, 'wb+') as destination:
+    with open('apps/static/upload' +f.name, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
