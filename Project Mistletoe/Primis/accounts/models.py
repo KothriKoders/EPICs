a77bcf2f98d292c02ce955f6367f873dc7643e55
Hershil Piplani <54028832+herkura@users.noreply.github.com>
2021-02-16 14:29:08 +0530

    Update models.py

--- Project Mistletoe/Primis/accounts/models.py
+++ Project Mistletoe/Primis/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class assignment(models.Model):
+    firstname = models.CharField("Enter you first name", maxlength=50)
+    lastname = models.CharField("Enter your last name", max_length=50)
+    file = models.FileField()  # for creating file input
+
